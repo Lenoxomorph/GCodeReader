28584acfc850e2d5c9e6e309cf5726e9bef27f04
Lenoxomorph <89936207+Lenoxomorph@users.noreply.github.com>
2023-08-04 11:51:52 -0400

    Initial Commit

--- src/GCodeReader/Reader.java
+++ src/GCodeReader/Reader.java
@@ -1,13 +1,68 @@
 package GCodeReader;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class Reader extends JFrame {
     private static final String WINDOW_TITLE = "GCodeReader";
     public Reader() {
         super(WINDOW_TITLE);
-        setContentPane(new JPanel());
+
+        JTextArea outputArea = new JTextArea() {{
+            setEditable(false);
+            setBorder(BorderFactory.createLineBorder(Color.black, 1, true));
+            setBackground(Color.decode("#f2fbff"));
+            setLineWrap(true);
+        }};
+        JTextArea inputArea = new JTextArea() {
+            private void update() {
+                outputArea.setText(getText());
+            }
+            {
+            setBorder(BorderFactory.createLineBorder(Color.black, 1, true));
+            setBackground(Color.decode("#f2fbff"));
+            setLineWrap(true);
+            getDocument().addDocumentListener(new DocumentListener() {
+                @Override
+                public void insertUpdate(DocumentEvent e) {update();}
+                @Override
+                public void removeUpdate(DocumentEvent e) {update();}
+                @Override
+                public void changedUpdate(DocumentEvent e) {update();}
+            });
+        }};
+        JButton importButton = new JButton("Import") {{
+            setFocusable(false);
+            setHorizontalAlignment(SwingConstants.RIGHT);
+            addActionListener(e -> {
+                JFileChooser chooser = new JFileChooser();
+                chooser.showOpenDialog(this);
+                File file = chooser.getSelectedFile();
+                System.out.println(file);
+            });
+        }};
+
+
+
+        setContentPane(new JPanel(new BorderLayout()) {{
+            add(new JPanel(new FlowLayout(FlowLayout.CENTER, 15, 15)) {{
+                add(importButton);
+            }}, BorderLayout.PAGE_START);
+            add(new JPanel() {{
+                add(inputArea);
+                add(outputArea);
+            }}, BorderLayout.CENTER);
+        }});
+
         pack();
+        //setSize(new Dimension((2 * WINDOW_GAP) + (5 * (NUM_SIZE + NUM_GAP)) + 38, 300)); // TODO: 8/2/2023
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
---
+++ src/GCodeReader/TestZone.java
@@ -0,0 +1,19 @@
+package GCodeReader;
+
+public class TestZone {
+    public static void main(String[] args) {
+        /*
+        try {
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+         */
+    }
+}
